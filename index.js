f9eb46187039197bcdcfe551b3aa154f44091850
Seth Fitzsimmons <seth@mojodna.net>
2013-11-03 14:16:51 -0800

    Cache getTile() calls

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var url = require("url"),
+    util = require("util");
+
 var LRU = require("lru-cache"),
     tilelive = require("tilelive");
 
@@ -10,9 +13,73 @@ var sourceCache = LRU({
   }
 });
 
-var sourceLocks = LRU({
-  maxAge: 5000 // effectively the load() timeout
+var tileCache = LRU({
+  max: 10 * 1024 * 1024, // 10MB
+  length: function(val) {
+    return val.data.length;
+  },
+  maxAge: 6 * 3600e3 // 6 hours
 });
+
+// NOTE: don't set a maxAge, otherwise the list of waiting callbacks will get
+// reset and they'll never be called.
+var sourceLocks = LRU(),
+    tileLocks = LRU();
+
+var cacheify = function(uri, source) {
+  uri = url.parse(uri);
+
+  var rawGetTile = source.getTile;
+
+  source.getTile = function(z, x, y, callback) {
+    var key = util.format("%s/%d/%d/%d", url.format(uri), z, x, y);
+
+    console.log(key);
+
+    var locks;
+    if ((locks = tileLocks.get(key))) {
+      console.log("%s locked", key);
+      locks.push(callback);
+      tileLocks.set(key, locks);
+      return;
+    }
+
+    var tile;
+    if ((tile = tileCache.get(key))) {
+      // cache hit!
+      return setImmediate(callback, null, tile.data, tile.headers);
+    }
+
+    // lock
+    tileLocks.set(key, [callback]);
+
+    console.log("fetching %s", key);
+    return rawGetTile.call(source, z, x, y, function(err, data, headers) {
+      // get the list of callbacks to notify
+      var waiting = tileLocks.get(key);
+
+      if (waiting.length > 1) {
+        console.log("%d waiting tile request(s).", waiting.length - 1);
+      }
+
+      // unlock
+      tileLocks.del(key);
+
+      if (!err) {
+        tileCache.set(key, {
+          data: data,
+          headers: headers
+        });
+      }
+
+      waiting.forEach(function(cb) {
+        return setImmediate(cb, err, data, headers);
+      });
+    });
+  };
+
+  return source;
+};
 
 var cache = Object.create(tilelive);
 
@@ -40,10 +107,15 @@ cache.load = function(uri, callback) {
     // get the list of callbacks to notify
     var waiting = sourceLocks.get(uri);
 
-    console.log("%d waiting request(s).", waiting.length);
+    if (waiting.length > 1) {
+      console.log("%d waiting request(s).", waiting.length - 1);
+    }
 
     // unlock
     sourceLocks.del(uri);
+
+    console.log("cacheifying %s", uri);
+    source = cacheify(uri, source);
 
     if (!err) {
       sourceCache.set(uri, source);
