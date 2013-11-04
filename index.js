2129ea21ace9c4e3fba2443b8152b2d5267f5de7
Seth Fitzsimmons <seth@mojodna.net>
2013-11-03 16:18:41 -0800

    Extract locking cache functionality as locking-cache

--- index.js
+++ index.js
@@ -3,128 +3,57 @@
 var url = require("url"),
     util = require("util");
 
-var LRU = require("lru-cache"),
+var lockingCache = require("locking-cache"),
     tilelive = require("tilelive");
 
-var sourceCache = LRU({
-  max: 5, // TODO arbitrary
-  dispose: function(key, source) {
-    source.close();
-  }
-});
-
-var tileCache = LRU({
-  max: 10 * 1024 * 1024, // 10MB
+// defined outside enableCaching so that a single, shared cache will be used
+// (this requires that keys be namespaced appropriately)
+var lockedGetTile = lockingCache({
+  max: 10 * 1024 * 1024, // TODO 10MB, should be configurable
   length: function(val) {
-    return val.data.length;
+    return (val[1] || "").length;
   },
   maxAge: 6 * 3600e3 // 6 hours
 });
 
-// NOTE: don't set a maxAge, otherwise the list of waiting callbacks will get
-// reset and they'll never be called.
-var sourceLocks = LRU(),
-    tileLocks = LRU();
-
-var cacheify = function(uri, source) {
+var enableCaching = function(uri, source) {
   uri = url.parse(uri);
 
   var rawGetTile = source.getTile;
 
-  source.getTile = function(z, x, y, callback) {
+  source.getTile = lockedGetTile(function(z, x, y, lock) {
     var key = util.format("%s/%d/%d/%d", url.format(uri), z, x, y);
 
-    console.log(key);
-
-    var locks;
-    if ((locks = tileLocks.get(key))) {
-      console.log("%s locked", key);
-      locks.push(callback);
-      tileLocks.set(key, locks);
-      return;
-    }
-
-    var tile;
-    if ((tile = tileCache.get(key))) {
-      // cache hit!
-      return setImmediate(callback, null, tile.data, tile.headers);
-    }
-
-    // lock
-    tileLocks.set(key, [callback]);
-
-    console.log("fetching %s", key);
-    return rawGetTile.call(source, z, x, y, function(err, data, headers) {
-      // get the list of callbacks to notify
-      var waiting = tileLocks.get(key);
-
-      if (waiting.length > 1) {
-        console.log("%d waiting tile request(s).", waiting.length - 1);
-      }
-
-      // unlock
-      tileLocks.del(key);
-
-      if (!err) {
-        tileCache.set(key, {
-          data: data,
-          headers: headers
-        });
-      }
-
-      waiting.forEach(function(cb) {
-        return setImmediate(cb, err, data, headers);
-      });
+    return lock(key, function(unlock) {
+      // .call is used so that getTile is correctly bound
+      return rawGetTile.call(source, z, x, y, unlock);
     });
-  };
+  }).bind(source);
 
   return source;
 };
 
 var cache = Object.create(tilelive);
 
-cache.load = function(uri, callback) {
-  var locks;
-  if ((locks = sourceLocks.get(uri))) {
-    // update the list of callbacks waiting for a source
-    // this is preferable to repeatedly using setImmediate and retrying
-    locks.push(callback);
-    sourceLocks.set(uri, locks);
-    return;
+var lockedLoad = lockingCache({
+  max: 5, // TODO arbitrary, should be configurable
+  dispose: function(key, source) {
+    source.close();
   }
+});
 
-  var source;
-  if ((source = sourceCache.get(uri))) {
-    // cache hit!
-    return setImmediate(callback, null, sourceCache.get(uri));
-  }
+cache.load = lockedLoad(function(uri, lock) {
+  var key = url.format(uri);
 
-  // lock
-  sourceLocks.set(uri, [callback]);
+  return lock(key, function(unlock) {
+    return tilelive.load(uri, function(err, source) {
+      if (!err) {
+        source = enableCaching(uri, source);
+      }
 
-  console.log("loading %s", uri);
-  return tilelive.load(uri, function(err, source) {
-    // get the list of callbacks to notify
-    var waiting = sourceLocks.get(uri);
-
-    if (waiting.length > 1) {
-      console.log("%d waiting request(s).", waiting.length - 1);
-    }
-
-    // unlock
-    sourceLocks.del(uri);
-
-    console.log("cacheifying %s", uri);
-    source = cacheify(uri, source);
-
-    if (!err) {
-      sourceCache.set(uri, source);
-    }
-
-    waiting.forEach(function(cb) {
-      return setImmediate(cb, err, source);
+      return unlock(err, source);
     });
   });
-};
+});
 
 module.exports = cache;
