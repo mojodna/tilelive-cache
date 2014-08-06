e9769f0a95964b4adae5ba460f4cf262acde2c3b
Seth Fitzsimmons <seth@mojodna.net>
2014-08-06 11:31:54 -0700

    Deal with present but undefined options.size

--- index.js
+++ index.js
@@ -72,7 +72,8 @@ var enableCaching = function(uri, source, locker) {
 
 module.exports = function(tilelive, options) {
   options = options || {};
-  options.size = "size" in options ? options.size : 10;
+  options.size = "size" in options && options.size ? options.size : null;
+  options.size = options.size || 10;
 
   // defined outside enableCaching so that a single, shared cache will be used
   // (this requires that keys be namespaced appropriately)
